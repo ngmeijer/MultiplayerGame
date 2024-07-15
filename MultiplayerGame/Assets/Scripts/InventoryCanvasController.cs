7ed24ef3f460b9c20244c15822fa87eac4658ea6
Nils Meijer <meijernils9@gmail.com>
2024-07-15 11:52:50 +0200

    Working on weapon creation & equip to inventory

--- MultiplayerGame/Assets/Scripts/InventoryCanvasController.cs
+++ MultiplayerGame/Assets/Scripts/InventoryCanvasController.cs
@@ -1,15 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryCanvasController : MonoBehaviour
 {
     [SerializeField] private GameObject _inventoryUI;
+    [SerializeField] private Image _helmetImage;
+    [SerializeField] private Image _upperbodyArmorImage;
+    [SerializeField] private Image _lowerbodyArmorImage;
     private bool _inventoryUIEnabled;
-    
+
+    private void Start()
+    {
+        _inventoryUI.SetActive(false);
+    }
+
     public void HandleInventoryCanvasState()
     {
         _inventoryUIEnabled = !_inventoryUIEnabled;
         _inventoryUI.SetActive(_inventoryUIEnabled);
+    }
+
+    public void UpdateInventoryCanvas(ArmorSettings pNewArmor)
+    {
+        switch (pNewArmor.Type)
+        {
+            case ArmorType.Helmet:
+                _helmetImage.sprite = pNewArmor.ArmorUISprite;
+                break;
+            case ArmorType.UpperbodyArmor:
+                _upperbodyArmorImage.sprite = pNewArmor.ArmorUISprite;
+                break;
+            case ArmorType.LowerBodyArmor:
+                _lowerbodyArmorImage.sprite = pNewArmor.ArmorUISprite;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 }
--- MultiplayerGame/Assets/Scripts/InventoryController.cs
+++ MultiplayerGame/Assets/Scripts/InventoryController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,13 +9,29 @@ public class InventoryController : MonoBehaviour
 {
     private PlayerControls _controls;
 
-    [SerializeField] private UnityEvent OnChangedInventoryState = new UnityEvent();
+    [SerializeField] private ArmorSettings _helmetEquipped;
+    [SerializeField] private ArmorSettings _upperBodyArmorEquipped;
+    [SerializeField] private ArmorSettings _lowerBodyArmorEquipped;
+
+    [SerializeField] private UnityEvent OnChangedInventoryEnabledState = new UnityEvent();
+    [SerializeField] private UnityEvent<ArmorSettings> OnChangedArmorEquipped = new UnityEvent<ArmorSettings>();
+    [SerializeField] private UnityEvent<WeaponSettings> OnChangedWeaponEquipped = new UnityEvent<WeaponSettings>();
 
     private void Awake()
     {
         _controls = new PlayerControls();
     }
-    
+
+    private void Start()
+    {
+        if (_helmetEquipped != null)
+            OnChangedArmorEquipped?.Invoke(_helmetEquipped);
+        if (_upperBodyArmorEquipped != null)
+            OnChangedArmorEquipped?.Invoke(_upperBodyArmorEquipped);
+        if (_lowerBodyArmorEquipped != null)
+            OnChangedArmorEquipped?.Invoke(_lowerBodyArmorEquipped);
+    }
+
     private void OnEnable()
     {
         _controls.UI.Enable();
@@ -29,6 +46,6 @@ public class InventoryController : MonoBehaviour
 
     private void HandleMovePerformed(InputAction.CallbackContext obj)
     {
-        OnChangedInventoryState?.Invoke();
+        OnChangedInventoryEnabledState?.Invoke();
     }
 }
